cee0a7d64566dcb4ee06a92efc9ca69d90d8745d
CoinFinance <75260320+CoinFinance@users.noreply.github.com>
2020-12-01 06:59:28 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CoinFinance Interface
---
+++ src/constants/v1/index.ts
@@ -0,0 +1,14 @@
+import { Interface } from '@ethersproject/abi'
+import { ChainId } from '@uniswap/sdk'
+import V1_EXCHANGE_ABI from './v1_exchange.json'
+import V1_FACTORY_ABI from './v1_factory.json'
+
+const V1_FACTORY_ADDRESSES: { [chainId in ChainId]: string } = {
+  [ChainId.MAINNET]: '0xadbfa35a7f806e997641faf90f6487859d1d08eb', //TODO
+  [ChainId.BSCTESTNET]: '0x685d9aC83e88b4CF488546abf245c2F6F7d4300e'
+}
+
+const V1_FACTORY_INTERFACE = new Interface(V1_FACTORY_ABI)
+const V1_EXCHANGE_INTERFACE = new Interface(V1_EXCHANGE_ABI)
+
+export { V1_FACTORY_ADDRESSES, V1_FACTORY_INTERFACE, V1_FACTORY_ABI, V1_EXCHANGE_INTERFACE, V1_EXCHANGE_ABI }
